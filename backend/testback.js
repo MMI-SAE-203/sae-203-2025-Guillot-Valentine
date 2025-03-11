e2c5b944aa1c17e2fdf7a140e2c5febf83e5c58e
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2025-03-11 10:54:00 +0100

    avancé astro

---
+++ backend/testback.js
@@ -0,0 +1,74 @@
+import { allFilm } from "./backend.mjs";
+import { oneFilm } from "./backend.mjs";
+import { allActivite } from "./backend.mjs";
+import { oneActivite } from "./backend.mjs";
+import { allInvite } from "./backend.mjs";
+import { oneInvite } from "./backend.mjs";
+import { allActiviteByInviteId } from "./backend.mjs";
+import { allActiviteByInviteNom } from "./backend.mjs";
+import { updateById } from "./backend.mjs";
+
+// try {
+// const records = await allFilm() ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await oneFilm('o9q5m53bkhjseys') ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await allActivite() ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await oneActivite('24j2oc2050015be') ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await allInvite() ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await oneInvite('axk6iu7r6865pmx') ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await allActiviteByInviteId('gtk11js4420k3xu') ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+// try {
+// const records = await allActiviteByInviteNom('Guérin') ;
+// console.log(JSON.stringify(records, null, 2)) ;
+// } catch (e) {
+// console.error(e) ;
+// }
+
+try {
+    const data = {
+        "nom" : "Laurent",
+    }
+    await updateById('invite',"axk6iu7r6865pmx", data) ;
+} catch (e) {
+    console.error(e) ;
+}
